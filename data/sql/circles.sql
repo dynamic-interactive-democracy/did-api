579ec1c01e02f2dcc5762da1881bf59e3a968b2a
Asbjørn Thegler <asbjoern@gmail.com>
2017-06-26 12:31:44 +0200

    Ensures unique circle names

--- data/sql/circles.sql
+++ data/sql/circles.sql
@@ -6,4 +6,6 @@ CREATE TABLE IF NOT EXISTS circles
     id uuid DEFAULT gen_random_uuid(),
     happened_at timestamp without time zone DEFAULT NOW(),
     PRIMARY KEY (id)
-)
+);
+
+CREATE UNIQUE INDEX IF NOT EXISTS circle_data_name_idx ON circles((data->>'name'));
